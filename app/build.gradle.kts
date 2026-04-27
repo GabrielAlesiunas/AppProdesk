507e4b7b84f3998c5e83d179b60e815759c1a839
GabrielAlesiunas <gabriel.alesiunas@yahoo.com.br>
2026-04-27 14:38:04 -0300

    Estilizando as telas

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,5 +47,6 @@ dependencies {
     implementation("androidx.cardview:cardview:1.0.0")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.recyclerview:recyclerview:1.3.2")
+    implementation(platform("com.google.firebase:firebase-bom:34.12.0"))
 
 }
